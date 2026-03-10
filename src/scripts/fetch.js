8f9e7b7c870834c11a0dcd6d34c4bd309bd16910
overlordka <kopary787@gmail.com>
2026-03-10 10:14:05 +0100

    feat: added fetch to energyConsumption

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -16,3 +16,47 @@ export async function fetchDevices() {
 
     return response.json();
 }
+
+export async function fetchCurrentEnergyConsumption() {
+    const currentWeather = await fetch("https://exercise.mobicom-pro.com/api/weather", {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+        }
+    });
+
+    if (!currentWeather.ok) {
+        const text = await currentWeather.text();
+        throw new Error(`HTTP error! status: ${currentWeather.status}, message: ${text}`);
+    }
+
+    const currentTime = await currentWeather.json();
+
+    const currentDate = currentTime.timestamp.split(" ")[0];
+    const [yearPart, monthPart, dayPart] = currentDate.split("-");
+    const year = Number(yearPart);
+    const month = Number(monthPart);
+    const previousYear = month === 1 ? year - 1 : year;
+    const previousMonth = month === 1 ? 12 : month - 1;
+    const currentDateFrom = `${previousYear}-${String(previousMonth).padStart(2, "0")}-${dayPart}`;
+
+
+    const getEnergyConsumption = await fetch(`https://exercise.mobicom-pro.com/api/statistics?device_id=38&from=${currentDateFrom}&to=${currentDate}`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+        }
+    });
+
+    if (!getEnergyConsumption.ok) {
+        const text = await getEnergyConsumption.text();
+        throw new Error(`HTTP error! status: ${getEnergyConsumption.status}, message: ${text}`);
+    }
+
+    const energyConsumption = await getEnergyConsumption.json();
+    console.log(energyConsumption);
+    console.log(currentDate);
+    console.log(currentDateFrom);
+}
